1257b8af50a7bd8a5d1a25914132648bbaa4a6f4
monster828 <aldenrshurtz@gmail.com>
2026-05-21 19:42:59 -0500

    Pinpoint pos get update

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Movement/PosGetters/PinpointPosGet.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/Movement/PosGetters/PinpointPosGet.java
@@ -1,10 +1,12 @@
 package org.firstinspires.ftc.teamcode.Utils.Movement.PosGetters;
 
+import com.pedropathing.ftc.localization.constants.PinpointConstants;
 import com.qualcomm.hardware.gobilda.GoBildaPinpointDriver;
 
 import org.firstinspires.ftc.robotcore.external.navigation.AngleUnit;
 import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 import org.firstinspires.ftc.teamcode.Utils.Movement.Position;
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
 public class PinpointPosGet extends PositionGetter {
 
@@ -12,24 +14,32 @@ public class PinpointPosGet extends PositionGetter {
 
     public PinpointPosGet(GoBildaPinpointDriver pinpoint) {
         this.pinpoint = pinpoint;
+        pinpoint.setEncoderResolution(Constants.pinpointConstants.encoderResolution);
+        pinpoint.setEncoderDirections(Constants.pinpointConstants.forwardEncoderDirection,
+                Constants.pinpointConstants.strafeEncoderDirection);
+
+        pinpoint.setOffsets(Constants.pinpointConstants.strafePodX,
+                Constants.pinpointConstants.forwardPodY,
+                Constants.pinpointConstants.distanceUnit);
+
+        pinpoint.resetPosAndIMU();
     }
 
     @Override
     public float[] getPos() {
-
         return new float[] {
-                (float) -pinpoint.getPosY(DistanceUnit.INCH),
+                (float) pinpoint.getPosY(DistanceUnit.INCH),
                 (float) pinpoint.getPosX(DistanceUnit.INCH),
-                (float) -pinpoint.getHeading(AngleUnit.DEGREES)
+                (float) pinpoint.getHeading(AngleUnit.DEGREES)
         };
     }
 
     @Override
     public Position getPosi() {
         return new Position(
-                (float) -pinpoint.getPosY(DistanceUnit.INCH),
+                (float) pinpoint.getPosY(DistanceUnit.INCH),
                 (float) pinpoint.getPosX(DistanceUnit.INCH),
-                (float) -pinpoint.getHeading(AngleUnit.DEGREES)
+                (float) pinpoint.getHeading(AngleUnit.DEGREES)
         );
     }
 
