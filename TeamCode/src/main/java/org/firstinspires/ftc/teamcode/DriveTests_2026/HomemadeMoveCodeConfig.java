2bfdd539611f5d9009abfda51017b2d8d7682e3c
RandomJoe77 <acbronstad@yahoo.com>
2026-06-04 17:26:23 -0700

    File selector

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTests_2026/HomemadeMoveCodeConfig.java
@@ -0,0 +1,62 @@
+package org.firstinspires.ftc.teamcode.DriveTests_2026;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.teamcode.Utils.GUI.RoFileSelector;
+import org.firstinspires.ftc.teamcode.Utils.GUI.RoGUI;
+import org.firstinspires.ftc.teamcode.Utils.MiscUtils;
+
+@TeleOp
+public class HomemadeMoveCodeConfig extends LinearOpMode {
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        RoGUI gui = new RoGUI(telemetry);
+        String path = MiscUtils.dataFolder;
+        gui.add(new RoFileSelector("Move: ",telemetry,path));
+        waitForStart();
+        while(opModeIsActive()) {
+            if(this.gamepad1.dpad_down) {
+                gui.down();
+                while(this.gamepad1.dpad_down)
+                {
+                    gui.render();
+                    telemetry.update();
+                }
+            }
+            if(this.gamepad1.dpad_up) {
+                gui.up();
+                while(this.gamepad1.dpad_up)
+                {
+                    gui.render();
+                    telemetry.update();
+                }
+            }
+            if(this.gamepad1.dpad_right) {
+                gui.right();
+                while(this.gamepad1.dpad_right)
+                {
+                    gui.render();
+                    telemetry.update();
+                }
+            }
+            if(this.gamepad1.dpad_left) {
+                gui.left();
+                while(this.gamepad1.dpad_left)
+                {
+                    gui.render();
+                    telemetry.update();
+                }
+            }
+            if(this.gamepad1.left_bumper) {
+                gui.interact();
+                while(this.gamepad1.left_bumper)
+                {
+                    gui.render();
+                    telemetry.update();
+                }
+            }
+        }
+    }
+}
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/GUI/RoDropdown.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/GUI/RoDropdown.java
@@ -4,7 +4,7 @@ import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 public class RoDropdown extends RoTab {
 
-    int selected;
+    int selected = -1;
 
     public RoDropdown(String label, Telemetry telemetry) {
         super(label, telemetry);
@@ -39,5 +39,30 @@ public class RoDropdown extends RoTab {
 
     public void addOption(String label) {
         this.gui.addLabel(label);
+    }
+
+    @Override
+    public void render(boolean selected) {
+        if(gui.getOutOf(0).equals(true)) {
+            open = false;
+        }
+        if(!open) {
+            String dat = "";
+            if (selected) {
+                dat += "▓" + label;
+            } else {
+                dat += " " + label;
+            }
+            if(this.selected != -1) {
+                dat += gui.getOutOf(this.selected);
+            }
+            while (dat.length() < 25) {
+                dat += "-";
+            }
+            dat += ">";
+            tem.addData("",dat);
+        } else {
+            gui.render();
+        }
     }
 }
