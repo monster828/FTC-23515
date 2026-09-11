6cabe0aebee5f9715b622e1d8b1409b68b76658d
CarterBels <Cablocks2019@gmail.com>
2026-09-10 19:29:02 -0500

    9/10/26 Challenges

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTests_2026/PPChallenge_2_9_10_26.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTests_2026/PPChallenge_2_9_10_26.java
@@ -9,7 +9,6 @@ import com.bylazar.telemetry.PanelsTelemetry;
 import com.bylazar.telemetry.TelemetryManager;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierCurve;
-import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
 import com.pedropathing.ivy.Command;
 import com.pedropathing.ivy.Scheduler;
@@ -19,9 +18,9 @@ import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 
 import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
 
-@Autonomous(name = "PPChallenge_1_9_10_26")
+@Autonomous(name = "PPChallenge_2_9_10_26")
 @Configurable
-public class PPChallenge_1_9_10_26 extends OpMode {
+public class PPChallenge_2_9_10_26 extends OpMode {
     private TelemetryManager panelsTelemetry;
     public Follower follower;
 
@@ -31,38 +30,30 @@ public class PPChallenge_1_9_10_26 extends OpMode {
         MainChain = follower.pathBuilder()
                 .addPath(
                         new BezierCurve(
-                                new Pose(9.000, 61.750),
-                                new Pose(10.131, 109.103),
-                                new Pose(15.202, 130.756),
-                                new Pose(70.218, 126.352)
+                                new Pose(133.000, 133.000),
+                                new Pose(81.515, 129.124),
+                                new Pose(83.185, 95.867),
+                                new Pose(67.361, 65.666)
                         )
                 )
                 .setTangentHeadingInterpolation()
                 .addPath(
                         new BezierCurve(
-                                new Pose(70.218, 126.352),
-                                new Pose(139.718, 138.419),
-                                new Pose(126.040, 69.756)
+                                new Pose(67.361, 65.666),
+                                new Pose(64.011, 44.042),
+                                new Pose(23.680, 5.243),
+                                new Pose(96.766, 14.841)
                         )
                 )
                 .setTangentHeadingInterpolation()
                 .addPath(
                         new BezierCurve(
-                                new Pose(126.040, 69.756),
-                                new Pose(140.901, 0.776),
-                                new Pose(71.057, 13.773)
+                                new Pose(96.766, 14.841),
+                                new Pose(136.413, 15.521),
+                                new Pose(117.926, 47.178)
                         )
                 )
-                .setTangentHeadingInterpolation()
-                .addPath(
-                        new BezierCurve(
-                                new Pose(71.057, 13.773),
-                                new Pose(16.752, 4.309),
-                                new Pose(11.855, 23.433),
-                                new Pose(12.000, 85.000)
-                        )
-                )
-                .setTangentHeadingInterpolation()
+                .setLinearHeadingInterpolation(Math.toRadians(0), Math.toRadians(135))
                 .build();
     }
 
