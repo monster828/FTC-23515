700f11b2cc51fb8c7b1c6fd42701f54c670f4caf
CarterBels <Cablocks2019@gmail.com>
2026-09-17 17:56:37 -0500

    Made code to test intake approach paths, writes data to file on robot

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriveTests_2026/POC_Movement_Test.java
@@ -0,0 +1,85 @@
+package org.firstinspires.ftc.teamcode.DriveTests_2026;
+
+import static com.pedropathing.api.Paths.*;
+
+import com.pedropathing.api.PoseFactory;
+import com.pedropathing.follower.Follower;
+import com.pedropathing.math.Pose;
+import com.pedropathing.paths.Path;
+import com.pedropathing.ivy.Command;
+import com.pedropathing.ivy.Scheduler;
+import static com.pedropathing.ivy.Scheduler.schedule;
+import static com.pedropathing.ivy.commands.Commands.*;
+import static com.pedropathing.ivy.groups.Groups.sequential;
+import static com.pedropathing.ivy.pedro.PedroCommands.follow;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+
+@Autonomous(name = "POC_Movement_Test", group = "Autonomous")
+public class POC_Movement_Test extends LinearOpMode {
+
+    private Follower follower;
+
+    private final PoseFactory poseFactory = PoseFactory.degrees();
+
+    private final Pose start = poseFactory.of(9, 9, 90);
+    private final Pose path1 = poseFactory.of(27.897, 116.5223, 90);
+    private final Pose point2 = poseFactory.of(116.2229, 83.5488, 159.5286);
+    private final Pose point3 = poseFactory.of(68.0732, 32.4087, 90);
+    private final Pose point4 = poseFactory.of(10, 10, 90);
+
+    // Autonomous routine
+    public Command autoRoutine() {
+        return sequential(
+                follow(follower, path1()),
+                follow(follower, path2()),
+                follow(follower, path3()),
+                follow(follower, path4())
+        );
+    }
+
+    @Override
+    public void runOpMode() {
+        Scheduler.reset();
+        follower = Constants.create(hardwareMap);
+        follower.setPose(start);
+        follower.update();
+
+        waitForStart();
+        schedule(autoRoutine());
+
+        while (opModeIsActive()) {
+            follower.update();
+            Scheduler.execute();
+
+            telemetry.addData("x", follower.pose().x());
+            telemetry.addData("y", follower.pose().y());
+            telemetry.addData("heading", follower.pose().heading());
+
+            if (follower.currentPath() != null) {
+                telemetry.addData("Current path distance remaining", follower.distanceToEndpoint());
+                telemetry.addData("Path number", follower.pathIndex());
+            }
+
+            telemetry.update();
+        }
+    }
+
+    public Path path1() {
+        return line(start, path1).constant(path1);
+    }
+
+    public Path path2() {
+        return line(path1, point2).reverseTangent();
+    }
+
+    public Path path3() {
+        return line(point2, point3).constant(point3);
+    }
+
+    public Path path4() {
+        return line(point3, point4).constant(point4);
+    }
+}
