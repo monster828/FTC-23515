abf10f13889c6091945a8f55ad063069ff017eca
RandomJoe77 <acbronstad@yahoo.com>
2026-09-19 08:48:48 -0700

    Added GamepadButton, which allows you to represent a button on a gamepad.

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Utils/GamepadButton.java
@@ -0,0 +1,52 @@
+package org.firstinspires.ftc.teamcode.Utils;
+
+import com.qualcomm.robotcore.hardware.Gamepad;
+
+import java.lang.reflect.Field;
+
+public class GamepadButton {
+
+    GButton b;
+    Gamepad g;
+
+    /**
+     * Create a class to represent a button on a gamepad.
+     * @param controller the gamepad.
+     * @param button the button.
+     */
+    public GamepadButton(Gamepad controller, GButton button) {
+        b = button;
+        g = controller;
+    }
+
+    public enum GButton {
+        left_stick_button,
+        right_stick_button,
+        dpad_up,
+        dpad_down,
+        dpad_left,
+        dpad_right,
+        a,
+        b,
+        x,
+        y,
+        guide,
+        start,
+        back,
+        left_bumper,
+        right_bumper
+    }
+
+    /**
+     * Check if the button on the gamepad is pressed.
+     * @return if pressed, false if exception is thrown.
+     */
+    public boolean isPressed() {
+        try {
+            Field f = g.getClass().getField(b.toString());
+            return (boolean)f.get(g);
+        } catch (Exception e) {
+            return false;
+        }
+    }
+}
